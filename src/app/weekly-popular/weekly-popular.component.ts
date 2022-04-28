3394855c0afd52f2393ef13df66c14a7dc7b2179
Veselin Petkov <90397928+veselinppetkov@users.noreply.github.com>
2022-04-28 13:50:13 +0300

    Owl carousel working

--- src/app/weekly-popular/weekly-popular.component.ts
+++ src/app/weekly-popular/weekly-popular.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Subscription } from 'rxjs';
 import { Product } from '../product/product.model';
 import { ProductService } from '../product/product.service';
@@ -9,12 +10,42 @@ import { ProductService } from '../product/product.service';
   styleUrls: ['./weekly-popular.component.css']
 })
 export class WeeklyPopularComponent implements OnInit {
-
   weekly: Product[] = []!;
-
   subscription: Subscription = null!;
+  customOptions: OwlOptions = {
+    nav: false,
+    dots: true,
+    margin: 10,
+  }
+  customOptions2: OwlOptions = {
+    nav: false,
+    dots: true,
+    margin: 20,
+    loop: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1200: {
+        items: 3,
+      },
+      1440: {
+        items: 4,
+      },
+    }
+  }
 
   constructor(private productService: ProductService) { };
+
 
   ngOnInit() {
     this.subscription = this.productService.productsChanged
